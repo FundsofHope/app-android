beccfff657d926473a72735d87f68e8957bcfa71
Aditya <addiittya2006@gmail.com>
2015-11-26 11:12:31 +0530

    Inital Commit

---
+++ app/src/main/java/org/fundsofhope/androidapp/activities/LoginActivity.java
@@ -0,0 +1,129 @@
+package org.fundsofhope.androidapp.activities;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.fundsofhope.androidapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    EditText _userName;
+    EditText _passWord;
+
+    Button _loginButton;
+    Button _signupButton;
+
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        _userName = (EditText) findViewById(R.id.input_user);
+        _passWord = (EditText) findViewById(R.id.input_password);
+
+        _loginButton = (Button) findViewById(R.id.btn_login);
+        _signupButton = (Button) findViewById(R.id.btn_signup);
+
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _userName.getText().toString();
+        String password = _passWord.getText().toString();
+
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+        Intent inte=new Intent(LoginActivity.this,SlidingActivity.class);
+        startActivity(inte);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String password = _passWord.getText().toString();
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passWord.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passWord.setError(null);
+        }
+
+        return valid;
+    }
+}
---
+++ app/src/main/java/org/fundsofhope/androidapp/activities/TransitionFirstActivity.java
@@ -0,0 +1,45 @@
+package org.fundsofhope.androidapp.activities;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+
+import org.fundsofhope.androidapp.R;
+import org.fundsofhope.androidapp.Utils;
+
+public class TransitionFirstActivity extends Activity {
+
+    private View fabButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition_first);
+
+        // Set explode animation when enter and exit the activity
+        Utils.configureWindowEnterExitTransition(getWindow());
+
+        // Fab Button
+        fabButton = findViewById(R.id.fab_button);
+        fabButton.setOnClickListener(fabClickListener);
+        Utils.configureFab(fabButton);
+    }
+
+
+    View.OnClickListener fabClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        Intent i  = new Intent (TransitionFirstActivity.this, TransitionSecondActivity.class);
+
+        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(TransitionFirstActivity.this,
+                Pair.create(fabButton, "fab"));
+
+        startActivity(i, transitionActivityOptions.toBundle());
+        }
+    };
+}
